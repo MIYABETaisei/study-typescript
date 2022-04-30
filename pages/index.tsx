6fc19f45ce0b371a98e058af2f27914afaf6a4c7
MIYABETaisei <mytspe46@gmail.com>
2022-04-30 18:07:40 +0900

    ✨ 型推論、型アノテーション、型アサーションについて

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 import type { NextPage } from 'next'
 
+let foo = {} as {bar: number};
+foo.bar = 1;
+
+// let foo: number = 123;
+
+function double(x : number) : number | undefined {
+  if(x > 0) {
+    return
+  }
+  return x*2;
+}
+
 const Home: NextPage = () => {
   return (
     <div className="test">test</div>
