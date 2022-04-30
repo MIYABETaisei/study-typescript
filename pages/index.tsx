ca50e4af5b28f0e9688000ff3bdbdc5973a36dd1
MIYABETaisei <mytspe46@gmail.com>
2022-04-30 18:20:11 +0900

    ✨ プリミティブ型、オブジェクト型を学びました

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
-let foo = {} as {bar: number};
-foo.bar = 1;
-
-// let foo: number = 123;
-
-function double(x : number) : number | undefined {
-  if(x > 0) {
-    return
-  }
-  return x*2;
-}
+const foo: boolean = true;
+const bar: undefined = undefined;
 
 const Home: NextPage = () => {
-  return (
-    <div className="test">test</div>
-  )
-}
+  return <div className="test">test</div>;
+};
 
-export default Home
+export default Home;
