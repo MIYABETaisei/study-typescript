7cbb881ed7325f4cd2ecfe44d938ce15569c990a
MIYABETaisei <mytspe46@gmail.com>
2022-05-09 23:21:15 +0900

    ✨

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
 
-const foo: boolean = true;
-const bar: undefined = undefined;
+// ダメ
+// let obj1: {} = {};
+// let obj2: object = {};
+// 推奨
+let obj3: Record<string, unknown> = {
+  a: 1, 
+  b: "foo",
+};
+let obj4: { [key: string]: unknown } = {};
 
 const Home: NextPage = () => {
-  return <div className="test">test</div>;
+  return <div>test</div>;
 };
-
-export default Home;
+export default Home
