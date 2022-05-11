e539df050ebde538a1be4cc3d325b785823b8d90
MIYABETaisei <mytspe46@gmail.com>
2022-05-11 09:21:15 +0900

    ✨

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,12 @@ let obj3: Record<string, unknown> = {
   a: 1, 
   b: "foo",
 };
-let obj4: { [key: string]: unknown } = {};
+// let obj4: { [key: string]: unknown } = {
+let obj4: { [key: string]: {foo: unknown} } = {
+  a: {
+    foo: 1,
+  }, 
+};
 
 const Home: NextPage = () => {
   return <div>test</div>;
