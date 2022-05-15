04cae5ea582dbbd885f0da154b4dc9f14b80c00d
MIYABETaisei <mytspe46@gmail.com>
2022-05-15 10:09:18 +0900

    ✨

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,29 @@ import type { NextPage } from "next";
 // let obj1: {} = {};
 // let obj2: object = {};
 // 推奨
-let obj3: Record<string, unknown> = {
-  a: 1, 
-  b: "foo",
+type Foo = {
+  a: number;
+  b: string;
 };
-// let obj4: { [key: string]: unknown } = {
-let obj4: { [key: string]: {foo: unknown} } = {
-  a: {
-    foo: 1,
-  }, 
+type Bar = {
+  a: string;
+  c: boolean;
 };
 
+type FooBar = Foo | Bar;
+
+const test: FooBar = {
+  a: "",
+  b: "",
+  c: true,
+};
+
+if ("b" in test) {
+  test.a.toFixed;
+} else {
+  test.a.length;
+}
 const Home: NextPage = () => {
   return <div>test</div>;
 };
-export default Home
+export default Home;
