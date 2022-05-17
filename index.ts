c13d9c1a5497686706a579ff7f4e6bcc7b8853bb
MIYABETaisei <mytspe46@gmail.com>
2022-05-17 21:50:39 +0900

    ✨ genericsについて

--- index.ts
+++ index.ts
@@ -1,20 +1,11 @@
-type UserA = { name: string; lang: "ja" };
-type UserB = { name: string; lang: "en" };
-
-// ユーザ定義のタイプガード
-const isUserA = (user: UserA | UserB): user is UserA => {
-  return user.lang === "ja";
+export type Foo<T extends string | number = string> = {
+  value: T;
 };
 
-const isUserB = (user: UserA | UserB): user is UserB => {
-  return user.lang === "en";
+const foo1: Foo<string> = {
+  value: "",
 };
 
-const users: (UserA | UserB)[] = [
-  { name: "たなか", lang: "ja" },
-  { name: "やまだ", lang: "ja" },
-  { name: "ジョニー", lang: "en" },
-];
-
-export const japanese = users.filter(isUserA);
-const notJapanese = users.filter(isUserB);
+const foo2: Foo<number> = {
+  value: 123,
+};
