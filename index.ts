3dc4ad5d718df70cfc965acd1b24c8ddb60252a3
MIYABETaisei <mytspe46@gmail.com>
2022-05-17 20:55:14 +0900

    ✨ mapped type

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
-export function getFirstLetter(str: number) {
-  return (str as unknown as string).charAt(0);
-}
+export type User = {
+  name: string;
+} & PersonalData;
+
+const foo = {
+  height: 200,
+  weight: 75,
+};
+
+type PersonalData = {
+  // height: number;
+  // weight: number;
+  [K in keyof typeof foo]?: number;
+};
+
+const user: User = {
+  name: "たいせい",
+  height: 185,
+  weight: 75,
+};
